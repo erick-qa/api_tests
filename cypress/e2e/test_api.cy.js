b8a6ac6e7c3dabbe8cfa61d82d6b4aac65b0615b
erick-qa <erick_gtr9@hotmail.com>
2024-12-17 21:27:02 -0300

    Update descriptionand remove comments

--- cypress/e2e/test_api.cy.js
+++ cypress/e2e/test_api.cy.js
@@ -1,41 +1,32 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 describe("GET /api/users", () => {
   let totalPages;
   let randomUserId;
 
-  // Teste para listar os usuários e armazenar o ID aleatório
-  it("Deve listar os usuários e armazenar o ID aleatório", () => {
+  it("List Users And Store Random ID", () => {
     cy.api("GET", "https://reqres.in/api/users?page=1").then((response) => {
       expect(response.status).to.eq(200);
-
-      // Armazenar o número total de páginas
       totalPages = response.body.total_pages;
-
-      // Validar os usuários da primeira página
       cy.validateUserProperties(response.body.data);
 
-      // Selecionar um ID aleatório da primeira página
       const randomUser = response.body.data[
         Math.floor(Math.random() * response.body.data.length)
       ];
-      randomUserId = randomUser.id; // Armazenar o ID aleatório
+      randomUserId = randomUser.id;
     });
   });
 
-  // Teste para validar o usuário específico com ID aleatório
-  it("Deve validar o usuário específico com ID aleatório", () => {
+  it("Validate Specific User With Random ID", () => {
     cy.api(`GET`, `https://reqres.in/api/users/${randomUserId}`).then(
       (userResponse) => {
         expect(userResponse.status).to.eq(200);
-        // Validar as propriedades do usuário com ID específico
-        cy.validateUserProperties([userResponse.body.data]); // Passando como um array para o comando customizado
+        cy.validateUserProperties([userResponse.body.data]);
       }
     );
   });
 
-  // Teste para validar todas as páginas de usuários
-  it("Deve validar todas as páginas de usuários", () => {
+  it("Validate All Pages Of Users", () => {
     for (let page = 2; page <= totalPages; page++) {
       cy.api("GET", `https://reqres.in/api/users?page=${page}`).then(
         (response) => {
@@ -46,86 +37,77 @@ describe("GET /api/users", () => {
     }
   });
 
-  it("Deve retornar erro 404 ao buscar um usuário não encontrado", () => {
+  it("Return Error 404 When Searching For A Non-Existent User", () => {
     cy.api({
-      method: 'GET',
-      url: 'https://reqres.in/api/users/23',
-      failOnStatusCode: false // Garante que o Cypress não falhe com o status 404
+      method: "GET",
+      url: "https://reqres.in/api/users/23",
+      failOnStatusCode: false,
     }).then((response) => {
-      expect(response.status).to.eq(404); // Espera o código de status 404
-      expect(response.body).to.be.empty; // O corpo da resposta deve ser vazio
+      expect(response.status).to.eq(404);
+      expect(response.body).to.be.empty;
     });
   });
 
-  it("Deve criar um usuário com nome e cargo aleatórios", () => {
-    // Gerar nome e cargo aleatórios
+  it("Create A User With Random Name And Job Title", () => {
     const randomName = faker.name.firstName();
     const randomJob = faker.name.jobTitle();
 
     const requestBody = {
       name: randomName,
-      job: randomJob
+      job: randomJob,
     };
 
     cy.api({
-      method: 'POST',
-      url: 'https://reqres.in/api/users',
-      body: requestBody
+      method: "POST",
+      url: "https://reqres.in/api/users",
+      body: requestBody,
     }).then((response) => {
-      expect(response.status).to.eq(201); // Verifica se o status é 201 (Created)
-      
-      // Verifica se o nome e o cargo enviados estão na resposta
-      expect(response.body).to.have.property('name', randomName);
-      expect(response.body).to.have.property('job', randomJob);
-
-      // Armazenando o ID do usuário criado para usar no teste de PATCH
-      Cypress.env('userId', response.body.id);
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property("name", randomName);
+      expect(response.body).to.have.property("job", randomJob);
+      expect(response.body).to.have.property("id");
+      Cypress.env("userId", response.body.id);
     });
   });
 
-  it("Deve atualizar o nome e o cargo do usuário", () => {
-    // Recuperando o ID do usuário criado anteriormente
-    const userId = Cypress.env('userId');
+  it("Update User's Name And Job Title", () => {
+    const userId = Cypress.env("userId");
 
-    // Dados para atualizar
     const updatedName = "morpheus";
     const updatedJob = "zion resident";
 
     const patchBody = {
       name: updatedName,
-      job: updatedJob
+      job: updatedJob,
     };
 
     cy.api({
-      method: 'PATCH',
-      url: `https://reqres.in/api/users/${userId}`, // Usando o ID armazenado
-      body: patchBody
+      method: "PATCH",
+      url: `https://reqres.in/api/users/${userId}`,
+      body: patchBody,
     }).then((patchResponse) => {
-      expect(patchResponse.status).to.eq(200); // Verifica se o status é 200 (OK)
-
-      // Verifica se os dados do usuário foram atualizados corretamente
-      expect(patchResponse.body).to.have.property('name', updatedName);
-      expect(patchResponse.body).to.have.property('job', updatedJob);
+      expect(patchResponse.status).to.eq(200);
+      expect(patchResponse.body).to.have.property("name", updatedName);
+      expect(patchResponse.body).to.have.property("job", updatedJob);
     });
   });
-  it("Deve deletar o usuário criado e atualizado", () => {
-    // Recuperando o ID do usuário criado e atualizado
-    const userId = Cypress.env('userId');
 
-    // Passo 1: Deletar o usuário
+  it("Delete The Created And Updated User", () => {
+    const userId = Cypress.env("userId");
+  
+    // Excluir o usuário
     cy.api({
-      method: 'DELETE',
-      url: `https://reqres.in/api/users/${userId}`, // Usando o ID do usuário
+      method: "DELETE",
+      url: `https://reqres.in/api/users/${userId}`,
     }).then((deleteResponse) => {
-      expect(deleteResponse.status).to.eq(204); // 204 No Content (sem corpo)
-
-      // Passo 2: Verificar se o usuário foi realmente deletado tentando buscar o usuário
+      expect(deleteResponse.status).to.eq(204);
+  
       cy.api({
-        method: 'GET',
+        method: "GET",
         url: `https://reqres.in/api/users/${userId}`,
-        failOnStatusCode: false // Não falhar no status de erro, pois esperamos 404
+        failOnStatusCode: false,
       }).then((getResponse) => {
-        expect(getResponse.status).to.eq(404); // Verifica que o usuário não foi encontrado
+        expect(getResponse.status).to.eq(404);
       });
     });
   });
