9984f093c33080a46a8333c3f229a0ac2f951290
erick-qa <erick_gtr9@hotmail.com>
2024-12-17 21:51:04 -0300

    Upload report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,20 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Configurar o reporter
+      require("cypress-mochawesome-reporter/plugin")(on);
+
+      // Retornar a configuração modificada
+      return config;
     },
+    // Outras configurações do Cypress
+    baseUrl: "https://reqres.in", // Defina o baseUrl da sua API ou aplicação
+    reporter: "cypress-mochawesome-reporter", // Usar o mochawesome como reporter
+    reporterOptions: {
+      reportDir: "cypress/reports",  // Local onde os relatórios serão salvos
+      overwrite: false,              // Não sobrescrever os relatórios existentes
+      html: true,                    // Gerar o relatório em HTML
+      json: true                     // Gerar o relatório em JSON
+    }
   },
 });
